a2dabb26ac6633bfd54ea68a05065f0b3317af03
julesndanga7@gmail.com <julesndanga7@gmail.com>
2021-06-14 19:55:08 +0100

    Premier livrable du 14/06/2021

---
+++ Ecoles/Ecoles/Ecole.cs
@@ -0,0 +1,59 @@
+﻿using System;
+/**
+ * Principe de d'heritage
+ * On creer La classe Person
+ * Les classes Eleve et Professeur hérites de Person
+ * **/
+//La classe Person
+class Person
+{
+    protected int age;
+    public void Bonjour()
+    {
+        Console.WriteLine("Bonjour");
+    }
+    public void SetAge(int n)
+    {
+        age = n;
+    }
+}
+//La classe Student
+class Eleve : Person
+{
+    public void AllerEnClasse()
+    {
+        Console.WriteLine("Je vais en classe");
+    }
+    public void AfficherAge()
+    {
+        Console.WriteLine("Mon age est: {0} ans", age);
+    }
+}
+//La classe Teacher
+class Professeur : Person
+{
+    private string cours;
+    public void Explicat()
+    {
+        Console.WriteLine("L'explication du cours commence");
+    }
+}
+//Classe de test
+class Ecole
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.Bonjour();
+        Eleve s = new Eleve();
+        s.AllerEnClasse();
+        s.SetAge(15);
+        s.Bonjour();
+        s.AfficherAge();
+        Professeur t = new Professeur();
+        t.SetAge(40);
+        t.Bonjour();
+        t.Explicat();
+        Console.ReadKey();
+    }
+}
---
+++ Examen/Examen/NoteExamen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+namespace LivrableOne
+{
+    /**
+     * calcule de la moyenned’une série de notes d’examen
+     * **/
+    class NoteExamen
+    {
+
+
+        /**
+         * Fonction qui calcule la moyenne des notes 
+         * 
+         * **/
+        public static float CalculMoyenNote(float[] notes)
+        {
+            float total = 0;
+            float moyen;
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                total = total + notes[i];
+            }
+            moyen = total / notes.Length;
+            return moyen;
+        }
+
+        /**
+         * Fonction qui Remplie les notes
+         * 
+         * **/
+        public static float[] Remplir(float[] notes)
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.WriteLine("Entrer la note n°" + i);
+                string note = Console.ReadLine();
+                notes[i] = float.Parse(note);
+            }
+            return notes;
+        }
+
+        /**
+         * Fonction qui affiche les notes
+         * 
+         * **/
+        public static void Affiche(float[] notes)
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.Write(" | " + notes[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            float[] notes;
+            float moyenne;
+            notes = new float[10];
+            notes = Remplir(notes);
+            Affiche(notes);
+
+            moyenne = CalculMoyenNote(notes);
+
+            Console.WriteLine("| La moyenne des ces notes est: " + moyenne);
+
+            /*
+             * J'utilise la fonction ReadKey à cause d'un problème d'affichage que j'ai eu
+             * La console se fermer immédiatement après être ouverte
+             * **/
+            Console.ReadKey(true); //attend que l'utilisateur appuie sur Entrée
+        }
+    }
+
+}
